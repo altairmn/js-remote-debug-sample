cd9b6780d82d5872a54dfe015735e9ce8e8709e5
Rhomi <contact@rhomi.xyz>
2023-11-01 19:52:38 -0700

    changed port

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0'
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, host, () => {
+  console.log(`Example app listening on PORT ${port}`)
 })
